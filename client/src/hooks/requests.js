7d5ba304a84ddad93aceef4a5cd538724318714c
Rom Fatal <39658012+RomFatal@users.noreply.github.com>
2023-03-29 06:58:58 +0300

    adding post launces to front

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -1,20 +1,32 @@
 const API_URL = 'http://localhost:8000';
 async function httpGetPlanets() {
-   const response = await fetch(`${API_URL}/planets`)
-   return   response.json();
+  const response = await fetch(`${API_URL}/planets`)
+  return response.json();
 }
 
 async function httpGetLaunches() {
   const response = await fetch(`${API_URL}/launches`)
   const fetchedLaunches = await response.json();
-   return fetchedLaunches.sort((a,b) =>{
+  return fetchedLaunches.sort((a, b) => {
     return a.flightNumber - b.flightNumber
-   });
+  });
 }
 
 async function httpSubmitLaunch(launch) {
-  // TODO: Once API is ready.
-  // Submit given launch data to launch system.
+  try {
+    return await fetch(`${API_URL}/launches`, {
+      method: "post",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(launch)
+    });
+  }
+  catch (err) {
+    return {
+      ok: false
+    };
+  }
 }
 
 async function httpAbortLaunch(id) {
--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -1,10 +1,25 @@
 const { getAllLaunches, addNewLaunches } = require('../../models/launches.model');
-function httpGetAllLaunches(req, res){
+function httpGetAllLaunches(req, res) {
     return res.status(200).json(getAllLaunches());
 }
 
-function httpAddNewLaunches(req, res){
+function httpAddNewLaunches(req, res) {
     const launch = req.body;
+
+    if (!launch.mission || !launch.rocket || !launch.launchDate
+        || !launch.target) {
+        return res.status(400).json({
+            error: 'Missing required launch property',
+        });
+    }
+
+    launch.launchDate = new Date(launch.launchDate);
+    if (isNaN(launch.launchDate)) {
+        return res.status(400).json({
+            error: 'Invalid launch date',
+        });
+    }
+
     launch.launchDate = new Date(launch.launchDate);
     addNewLaunches(launch);
     return res.status(201).json(launch);
