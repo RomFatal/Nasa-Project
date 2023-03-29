eaede0ad09fbee77ea6b9c647bad1cbdbd5b00ec
Rom Fatal <39658012+RomFatal@users.noreply.github.com>
2023-03-29 04:20:55 +0300

    adding morgan for API Logs

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const morgan = require('morgan');
 const cors = require('cors');
 const planetsRouter = require('./routes/planets/planets.router');
 
@@ -8,6 +9,7 @@ const app = express();
 app.use(cors({
     origin: 'http://localhost:3000'
 }))
+app.use(morgan('combined'))
 
 app.use(express.json());
 
