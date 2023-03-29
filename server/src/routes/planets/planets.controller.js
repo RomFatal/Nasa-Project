eb9ed68041d97cea87204fb68f10d5f413d136cf
Rom Fatal <39658012+RomFatal@users.noreply.github.com>
2023-03-29 03:42:48 +0300

    fetching data from file

--- server/src/routes/planets/planets.controller.js
+++ server/src/routes/planets/planets.controller.js
@@ -1,6 +1,6 @@
-const planets = require('../../models/planets.model.js');
+const { planets } = require('../../models/planets.model.js');
 function getAllPlanets(req, res){
-    return res.status(200).json(planets)
+    return res.status(200).json(planets);
 }
 
 module.exports = {
