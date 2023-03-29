4188da97788013fe0ce2bb426193591bee610f4a
Rom Fatal <39658012+RomFatal@users.noreply.github.com>
2023-03-29 06:08:51 +0300

    adding get lauches

---
+++ server/src/routes/launches/launches.router.js
@@ -0,0 +1,8 @@
+var express = require('express');
+const { httpGetAllLaunches } = require('./launches.controller');
+
+const launchesRouter = express.Router();
+
+launchesRouter.get('/launches', httpGetAllLaunches);
+
+module.exports = launchesRouter;
