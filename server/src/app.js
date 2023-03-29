a3dbcd044fe771e3eb4919d3d685cb71517d6a5e
Rom Fatal <39658012+RomFatal@users.noreply.github.com>
2023-03-29 04:20:31 +0300

    prepering for deploment

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const planetsRouter = require('./routes/planets/planets.router');
 
@@ -9,6 +10,9 @@ app.use(cors({
 }))
 
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
 app.use(planetsRouter);
 
 
