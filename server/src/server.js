a59ba951091f6aaa03cd164836397891c8404e06
Rom Fatal <39658012+RomFatal@users.noreply.github.com>
2023-04-08 21:51:06 +0300

    move mongoDB implementation to new module

--- server/src/server.js
+++ server/src/server.js
@@ -2,23 +2,16 @@ const http = require('http');
 const mongoose = require('mongoose');
 
 const app = require('./app');
+const { mongoConnect } = require('./services/mongo');
 
 const { loadPlanetsData } = require('./models/planets.model.js');
 
 const PORT = process.env.PORT || 8000;
 
-const MONGO_URL = 'mongodb+srv://nasa-api:FARfOHy5c80YgUuu@nasacluster.imejyrt.mongodb.net/nasa?retryWrites=true&w=majority';
-
 const server = http.createServer(app);
-mongoose.connection.once('open',()=>{
-    console.log("Ready");
-});
-mongoose.connection.on('error',(err)=>{
-    console.error (err)
-});
+
 async function startServer() {
-    await mongoose.connect(MONGO_URL);
-      
+    await mongoConnect();
     await loadPlanetsData();
     server.listen(PORT, () => {
         console.log(`Listening on port ${PORT}...`)
