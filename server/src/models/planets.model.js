8960276818778c8bc29a92f5fa09db6017cfae2d
Rom Fatal <39658012+RomFatal@users.noreply.github.com>
2023-04-08 19:39:11 +0300

    adding MongoDB

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,44 +1,61 @@
-
 const { parse } = require('csv');
 const fs = require('fs');
 const path = require('path')
+
+const planets = require('./planets.mongo')
+
 function isHabitablePlanet(planet) {
-    return planet['koi_disposition'] == 'CONFIRMED' 
-    && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
-    && planet['koi_prad'] < 1.6;
+    return planet['koi_disposition'] == 'CONFIRMED'
+        && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
+        && planet['koi_prad'] < 1.6;
 }
-const habitablePlanets = [];
- 
-function loadPlanetsData (){
-    return new Promise((resolve,reject)=>{
-     fs.createReadStream(path.join(__dirname, '..', '..', 'data', 'kepler_data.csv'))
-    .pipe(parse({
-        comment: '#',
-        columns: true,
-    }))
-    .on('data', (data) => {
-        if (isHabitablePlanet(data))
-            habitablePlanets.push(data);
-    })
-    .on('error', (err) => {
-        console.log(err);
-        reject(err);
-    })
-    .on('end', () => {
-         console.log(`${habitablePlanets.length} Habitable Planets Found!`)
-         resolve();
+function loadPlanetsData() {
+    return new Promise((resolve, reject) => {
+        fs.createReadStream(path.join(__dirname, '..', '..', 'data', 'kepler_data.csv'))
+            .pipe(parse({
+                comment: '#',
+                columns: true,
+            }))
+            .on('data', async (data) => {
+                if (isHabitablePlanet(data))
+                await savePlanet(data);
+            })
+            .on('error', (err) => {
+                console.log(err);
+                reject(err);
+            })
+            .on('end', async () => {
+                const countPlanetsFound = (await getAllPlanets()).length
+                console.log(`${countPlanetsFound} Habitable Planets Found!`)
+                resolve();
+            });
     });
-});
 }
 
-const promise = new Promise((resolve,reject)=>{
+const promise = new Promise((resolve, reject) => {
     resolve(42);
 });
-promise.then((result) =>{
+promise.then((result) => {
 })
 
-function getAllPlanets(){
-    return habitablePlanets;
+async function getAllPlanets() {
+    return await planets.find({},{
+        '_id':0, '__v':0,
+    });
+}
+
+async function savePlanet(planet){
+try {
+        await planets.updateOne({
+            keplerName: planet.kepler_name
+        }, {
+            keplerName: planet.kepler_name
+        }, { 
+            upsert: true
+        });
+} catch (err) {
+    console.error(`Could not save planet ${err}`);
+}
 }
 module.exports = {
     loadPlanetsData,
--- server/src/server.js
+++ server/src/server.js
@@ -1,13 +1,24 @@
 const http = require('http')
+const mongoose = require('mongoose')
 
 const app = require('./app')
 
 const { loadPlanetsData } = require('./models/planets.model.js')
+
 const PORT = process.env.PORT || 8000;
 
-const server = http.createServer(app);
+const MONGO_URL = 'mongodb+srv://nasa-api:FARfOHy5c80YgUuu@nasacluster.imejyrt.mongodb.net/nasa?retryWrites=true&w=majority'
 
+const server = http.createServer(app);
+mongoose.connection.once('open',()=>{
+    console.log("Ready")
+})
+mongoose.connection.on('error',(err)=>{
+    console.error (err)
+})
 async function startServer() {
+    await mongoose.connect(MONGO_URL);
+      
     await loadPlanetsData();
     server.listen(PORT, () => {
         console.log(`Listening on port ${PORT}...`)
